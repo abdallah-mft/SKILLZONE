3ecad1bb4456d64634f9785ed0112eeef93feaa4
Meftah Wassim Abdallah <w_meftah@estin.dz>
2025-03-05 13:18:26 +0100

    Update URL routing for users app: add registration, login, and profile endpoints

--- skillzone/users/urls.py
+++ skillzone/users/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path 
-from . import views 
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('users/',views.index , name = 'index') ,
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('profile/', views.get_profile, name='profile'),
 ]

02694afb7a882a1fdf239efa0c93efc30420986b
Meftah Wassim Abdallah <w_meftah@estin.dz>
2025-04-09 16:14:16 +0100

    urls

--- skillzone/skillzone/urls.py
+++ skillzone/skillzone/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
+from django.http import JsonResponse
+
+def test_view(request):
+    return JsonResponse({"message": "API is working!"})
 
 urlpatterns = [
+    path('', test_view, name='test'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/v1/', include([

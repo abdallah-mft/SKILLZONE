42107523452faf34aff222a69c85982269db12f4
Meftah Wassim Abdallah <w_meftah@estin.dz>
2025-04-13 20:50:21 +0100

    Add email settings for SMTP configuration in settings.py

--- skillzone/skillzone/settings.py
+++ skillzone/skillzone/settings.py
@@ -277,3 +277,12 @@ if 'test' in sys.argv:
             'NAME': BASE_DIR / 'test_db.sqlite3',
         }
     }
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')

7ba8ff258c037ffac03c40e79cd20148da59d8f1
Meftah Wassim Abdallah <w_meftah@estin.dz>
2025-05-11 17:21:06 +0100

    Add UnlockedCourse and UnlockedLesson models to views.py imports

--- skillzone/courses/views.py
+++ skillzone/courses/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from .models import Course, Lesson
+from .models import Course, Lesson, UnlockedCourse, UnlockedLesson
 from .serializers import CourseSerializer, LessonSerializer
 
 @api_view(['GET'])
